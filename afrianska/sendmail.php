d9336294a31bf8220461ab26709009c7b6e452b7
LeraKill <kilinav25@gmail.com>
2021-10-25 19:27:35 +0500

    change .php doc

--- afrianska/sendmail.php
+++ afrianska/sendmail.php
@@ -1,46 +1,13 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
+$recepient = "kilinav25@gmail.com";
+$siteName = "Ajax-форма";
 
-$mail = new PHPMailer(true);
-$mail->Charset = 'UTF-8';
-$mail->setLanguage('en', 'phpmailer/language/');
-$mail->isHTML(true);
+$name = trim($_POST["name"]);
+$phone = trim($_POST["email"]);
+$message = "Name: $name \nEmail: $email";
 
-//От кого письмо
-$mail->setForm('kilinav25@gmail.com', 'LeraKill');
-//Кому отправить
-$mail->addAddress('kilinav25@gmail.com');
-//Тело письма
-$mail->Subject = 'Hello!';
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 
-//Тело письма
-$body = '<h1>Here is my letter to you</h1>';
-
-if(trim(!empty($_POST['name']))){
-	$body.='<p><strong>Name:</strong>' .$_POST['name']. '</p>';
-}
-if(trim(!empty($_POST['email']))){
-	$body.='<p><strong>Email:</strong>' .$_POST['email']. '</p>';
-}
-if(trim(!empty($_POST['text']))){
-	$body.='<p><strong>Message:</strong>' .$_POST['text']. '</p>';
-}
-
-$mail->Body = $body;
-
-//Отправляем
-if(!$mail->send()){
-	$message ='Error';
-} else{
-	$message = 'Form sended';
-}
-
-$response = ['message'=>$message];
-
-header('Content-type:application/json');
-echo json_encode($response);
 ?>
